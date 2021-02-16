150aaa6ce43fa11d462467e26ff5872845a212bd
Saba Teklu <saab.teklu@gmail.com>
2021-02-16 10:06:30 -0700

    updated mongooseconnect

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/apologies', {
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/apologies', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true,
 })
   .then((results) => results)
